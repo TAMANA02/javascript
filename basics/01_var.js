c0dc4aac5c861ad7151fc1ee75977318f562728c
TAMANA02 <tamanashukla021@gmail.com>
2024-12-14 16:22:57 +0000

    learned the vriable ,constant and let in var.js

---
+++ basics/01_var.js
@@ -0,0 +1,11 @@
+const accountId="121214"
+var accountEmail="school@google.com"
+let accountPassword="2345"
+accountCity="jaipur"
+
+accountEmail="gfg@.com"
+accountPassword="1493"
+accountCity="delhi"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
