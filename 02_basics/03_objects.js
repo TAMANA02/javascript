421f234c797dc069e5bd7366ee1b223b7e8bdd1a
TAMANA02 <tamanashukla021@gmail.com>
2025-11-30 20:16:25 +0000

    learned objects basics

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,13 +2,36 @@
 
 // object literals 
 
-const JsUser{
-    name = "tamana",
-    age = 20,
-    location = "kanpur",
-    email = "tamana@gamil.com",
-    IsLoggedIn = false,
-    lastLoggedIn = ["monday","wednesday"]
+const Mysym  = Symbol("key1")
+
+const JsUser= {
+    name : "tamana",
+    "lastname" : "Shukla",
+    [Mysym] : "mykey1",
+    age : 20,
+    location : "kanpur",
+    email : "tamana@gamil.com",
+    IsLoggedIn : false,
+    lastLoggedIn : ["monday","wednesday"]
 }
 
-console.log(JsUser.email)
+//console.log(JsUser.email)
+//console.log(JsUser["name"])//-->tamana
+//console.log(JsUser["lastname"])//-->shukla
+//console.log(JsUser[Mysym])
+
+JsUser.email = "perplexity@gmail.com"
+//Object.freeze(JsUser)
+JsUser.email = "microsoft@gmail.com"
+//console.log(JsUser)
+
+JsUser.greeting = function(){
+   console.log("hello Js User")
+}
+
+
+JsUser.greetingTwo = function(){
+    console.log(`hello js user, ${this.email}`)
+}
+JsUser.greeting()
+JsUser.greetingTwo()
